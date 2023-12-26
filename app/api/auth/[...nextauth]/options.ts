99357f17449bf722ab3b304f19bd1dadf2f21a23
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2023-12-27 01:06:50 +0530

    work on jwt

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -7,7 +7,49 @@ export const options = {
             profile(profile) {
                 console.log("github profile", profile);
 
-            }
+                let userRole = "GitHub User";
+
+                if (profile?.email === "rahulbanik98@gmail.com") {
+                    userRole = "admin";
+                }
+
+                return {
+                    ...profile,
+                    role: userRole,
+                }
+            },
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_Secret,
+        }),
+        GoogleProvider({
+            profile(profile) {
+                console.log("Google profile", profile);
+
+                return {
+                    ...profile,
+                    role: userRole,
+                    id: profile.sub,
+                };
+            },
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_Secret,
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role;
+            return token;
+        },
+        // FOR CLIENTSIDE
+        async session({ session, token }) {
+            if (session?.user) session.user.role = token.user;
+            return session;
+        }
+    }
 }
+
+// return {
+//     ...profile,
+//     id: profile.sub,
+//     role: userRole
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello world 20</h1>
+      <h1>Hello world 25</h1>
     </>
   )
 }
